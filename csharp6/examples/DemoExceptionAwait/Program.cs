7043df16f3e3db2d6f8fc155a3aa67af266cd8b5
Michael Tsai <huanlin.tsai@gmail.com>
2022-02-09 19:09:41 +0800

    Add examples.

---
+++ csharp6/examples/DemoExceptionAwait/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DemoExceptionAwait
+{
+    class Program
+    {
+        // 注意：C# 7.1 以後才支援 async Main。
+        public static async Task Main() 
+        {
+            var s = await HttpGetStrAsync("http://localhost:12345");
+            Console.WriteLine(s);
+        }
+
+        static async Task<string> HttpGetStrAsync(string url)
+        {
+            var client = new System.Net.Http.HttpClient();
+            var streamTask = client.GetStringAsync(url);
+            try
+            {
+                var responseText = await streamTask;
+                return responseText;
+            }
+            catch (System.Net.Http.HttpRequestException ex)
+            {
+                await LogAsync($"失敗: {ex.Message}");
+                return ex.Message;
+            }
+            finally
+            {
+                client.Dispose();
+                await LogAsync("離開 HttpGHetAsync() 方法。");
+            }
+        }
+
+        static async Task LogAsync(string s)
+        {
+            // 一般來說，此函式不應該再拋出任何例外。
+            await File.AppendAllTextAsync(@"c:\temp\log.txt", s);
+        }
+    }
+}
---
+++ csharp6/examples/DemoExceptionFilter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DemoExceptionFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UpdateData();
+            }
+            catch (FakeSqlException ex) when (ex.Number == 2627)
+            {
+                Console.WriteLine("主鍵重複!");
+            }
+            catch (FakeSqlException ex) when (ex.Number == 1205)
+            {
+                Console.WriteLine("Deadlock!");
+            }
+        }
+
+        static void UpdateData()
+        {
+            throw new FakeSqlException() { Number = 1205 };
+        }
+    }
+
+    class FakeSqlException : Exception
+    {
+        public int Number { get; set; }
+    }
+}
